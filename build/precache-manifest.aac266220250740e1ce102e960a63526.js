be20dfd4c9f5f3f75b25a50f21a824ee8c152f7e
Rusky <jpah.design@gmail.com>
2020-11-20 15:24:26 -0500

    creating repo

---
+++ build/precache-manifest.aac266220250740e1ce102e960a63526.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347a037554d6389c1d7de71f74ce5dc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a060e055746cb2713d10",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a060e055746cb2713d10",
+    "url": "/static/js/main.e6e467ee.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
